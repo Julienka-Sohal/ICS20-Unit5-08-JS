d26e424b31d67b07b8141a610a76ee7279e173a2
Julienka-Sohal <Julienka-Sohal@users.noreply.github.com>
2022-12-14 21:11:21 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -25,11 +25,11 @@ function myButtonClicked() {
 
   while (answer >= secondNumber) {
     answer = answer - secondNumber
-    if (counter = answer - 1) {
+    if ((counter = answer - 1)) {
       let answerTwo = parseInt(document.getElementById("ANSTWO").value)
     }
-      document.getElementById("ANS").innerHTML = "The answer is " + answerTwo + " R " +answer + "."
-        counter++
+    document.getElementById("ANS").innerHTML =
+      "The answer is " + answerTwo + " R " + answer + "."
+    counter++
   }
-  
 }
